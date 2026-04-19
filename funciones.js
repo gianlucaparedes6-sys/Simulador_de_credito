633907087a8ef7b649b55268772559aba48e6ead
gianlucaparedes6-sys <gianlucaparedes6@gmail.com>
2026-04-19 10:39:43 -0500

    simulador de credito final

--- funciones.js
+++ funciones.js
@@ -1,1 +1,34 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos) {   
+let disponible = ingresos - egresos;
+
+    if (disponible < 0) {
+        return 0;
+    }
+
+    return disponible;
+}
+function calcularCapacidadPago(montoDisponible) {
+    let capacidadPago = montoDisponible * 0.5;
+    return capacidadPago;
+}
+function calcularInteresSimple(monto, tasa, plazo) {
+    let interes = monto * (tasa/100) * plazo;
+    return interes;
+}
+function calcularTotalPagar(monto, interes) {
+    let totalPagar = (monto + interes)+100;
+    return totalPagar;
+}
+function calcularCuotaMensual(totalPagar, plazo) {
+    let meses = plazo * 12;
+    let cuotaMensual = totalPagar / meses;
+    return cuotaMensual;
+}
+function aprobarCredito(capacidadPago, cuotaMensual) {
+    if (capacidadPago >= cuotaMensual) {
+        return "APROBADO";
+    } else {
+        return "RECHAZADO";
+    }   
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,34 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTMLfunction calcularDisponible(ingresos, egresos) {
+ function calcular(){
+    //recuperamos los componentes de las cajas de textos
+    //recuperamos los valores de los componentes todo esto desde utilitarios.js
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+    //disponible
+    let disponible = calcularDisponible(ingresos,egresos);
+    disponible = parseFloat(disponible.toFixed(2));
+    document.getElementById("spnDisponible").textContent = disponible;
+    //capacidad de pago
+    let capacidadPago = calcularCapacidadPago(disponible);
+    capacidadPago = parseFloat(capacidadPago.toFixed(2));
+    document.getElementById("spnCapacidadPago").textContent = capacidadPago;
+    //interes simple
+    let monto = parseFloat(document.getElementById("txtMonto").value);
+    let tasa = parseInt(document.getElementById("txtTasaInteres").value);
+    let plazo = parseInt(document.getElementById("txtPlazo").value);
+    let interes = calcularInteresSimple(monto,tasa,plazo);
+    interes = parseFloat(interes.toFixed(2));
+    document.getElementById("spnInteresPagar").textContent = interes;
+    //total a pagar
+    let totalPagar = calcularTotalPagar(monto,interes);
+    totalPagar = parseFloat(totalPagar.toFixed(2));
+    document.getElementById("spnTotalPrestamo").textContent = totalPagar;
+    //cuota mensual
+    let cuotaMensual = calcularCuotaMensual(totalPagar,plazo);
+    cuotaMensual = parseFloat(cuotaMensual.toFixed(2));
+    document.getElementById("spnCuotaMensual").textContent = cuotaMensual;
+    //estado del credito
+    let estadoCredito = aprobarCredito(capacidadPago,cuotaMensual);
+    document.getElementById("spnEstadoCredito").textContent = estadoCredito;
+
+}
